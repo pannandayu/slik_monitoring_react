5babd5e7ccd3aebd9b66f563e2fb41f2dcbe4bf4
Pannanda Liko Yu <poligoncf22@gmail.com>
2023-11-27 13:54:39 +0700

    tes client side env variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,62 @@
-import React, { useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import Title from "@/components/Title";
 import FormSwitch from "@/wrappers/FormSwitch";
 import DashboardSwitch from "@/wrappers/DashboardSwitch";
+import { motion } from "framer-motion";
+import Input from "@/components/Input";
+import styles from "@/styles/Input.module.css";
 
-export default function Home() {
+export default function Home(props: { password: string }) {
   const [formIsPG, setFormIsPG] = useState<boolean>(true);
+
+  const [auth, setAuth] = useState<boolean>(false);
+
+  const authHandler: React.FormEventHandler = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(props.password);
+  };
+
+  const authRef = useRef<HTMLInputElement>(null);
 
   const setFormIsPGHandler = (state: boolean) => {
     setFormIsPG(state);
   };
 
-  return (
+  return !auth ? (
+    <div
+      style={{
+        height: "50vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <form onSubmit={authHandler}>
+        <Input
+          labelName="Whisper me the magic words"
+          idForName="auth"
+          type="password"
+          className={styles.input}
+          ref={authRef}
+        />
+        <motion.button
+          style={{
+            backgroundColor: "#4caf50",
+            color: "white",
+            cursor: "pointer",
+            border: "none",
+            margin: "20px",
+            padding: "10px",
+            borderRadius: "8px",
+          }}
+          whileHover={{ backgroundColor: "#91c493", scale: 1.1 }}
+          whileTap={{ backgroundColor: "#91c493", scale: 0.9 }}
+        >
+          Submit
+        </motion.button>
+      </form>
+    </div>
+  ) : (
     <div>
       <h1>Find your client here.</h1>
       <h2>You can search by...</h2>
@@ -24,4 +70,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      password: process.env.PASSWORD,
+    },
+  };
 }
